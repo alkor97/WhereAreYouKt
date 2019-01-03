a815c551341bcec2adfe4338aaea0e4463955582
alkor97 <alkor@konto.pl>
2019-01-03 14:02:26 +0100

    Unused classes removed.

--- app/src/main/java/info/alkor/whereareyou/model/action/DurationProgress.kt
+++
@@ -1,6 +0,0 @@
-package info.alkor.whereareyou.model.action
-
-import info.alkor.whereareyou.common.Duration
-
-data class DurationProgress(val value: Duration)
-data class DurationCompleted(val value: Duration)
