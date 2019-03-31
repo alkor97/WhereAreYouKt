f7eb6faa3758fd835d3f4c38bb75d26eed435f1d
alkor97 <alkor@konto.pl>
2019-03-31 17:04:16 +0200

    Additional logging.

--- app/src/main/java/info/alkor/whereareyou/impl/location/AbstractLocationProvider.kt
+++ app/src/main/java/info/alkor/whereareyou/impl/location/AbstractLocationProvider.kt
@@ -1,5 +1,6 @@
 package info.alkor.whereareyou.impl.location
 
+import android.util.Log
 import info.alkor.whereareyou.api.location.LocationProvider
 import info.alkor.whereareyou.common.Duration
 import info.alkor.whereareyou.model.location.Location
@@ -13,12 +14,16 @@ abstract class AbstractLocationProvider(
         private val coroutineContext: CoroutineContext = DefaultDispatcher
 ) : LocationProvider {
 
+    private val loggingTag = "locating"
+
     override fun getLocation(timeout: Duration, maxAge: Duration, callback: (location: Location?, final: Boolean) -> Unit) {
         val totalTimeout = maxAge + timeout
         val deferred = providers.map {
             async(coroutineContext) {
+                Log.i(loggingTag, "$it queried")
                 val location = requestLocation(it)
                 if (location != null && location.time.notOlderThan(totalTimeout)) {
+                    Log.i(loggingTag, "$it responded with $location")
                     callback(location, false)
                 }
                 location
@@ -26,6 +31,7 @@ abstract class AbstractLocationProvider(
         }
         launch {
             withTimeoutOrNull(timeout.value, timeout.unit) {
+                Log.i(loggingTag, "awaiting location...")
                 deferred.awaitAll()
             }
             val location = deferred.asSequence().filter { it.isCompleted }
@@ -33,6 +39,7 @@ abstract class AbstractLocationProvider(
                     .filter { it.time.notOlderThan(totalTimeout) }
                     .sortedWith(compareBy<Location, Double?>(nullsLast()) { it.coordinates.accuracy?.value })
                     .firstOrNull()
+            Log.i(loggingTag, "got final location $location")
             callback(location, true)
         }
     }
